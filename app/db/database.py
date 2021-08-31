fde9bb40b7a1e6dec1761e3f701b6ff4ed6dae01
Naol Dame <naododa5566@gmail.com>
2021-09-01 01:55:17 +0300

    database created

--- app/db/database.py
+++ app/db/database.py
@@ -1,3 +1,29 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import Session
+from sqlalchemy.sql.expression import false
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mike123@localhost:3306/edir"
+
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+def  get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+def create_database():
+    return Base.metadata.create_all(bind=engine)
+
+    
+    
