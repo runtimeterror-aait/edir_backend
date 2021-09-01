e9000eb4b6bff4312a37e4338a3b4d6e6f356674
Naol Dame <naododa5566@gmail.com>
2021-09-01 17:39:48 +0300

    Edir definition..

--- app/schemas.py
+++ app/schemas.py
@@ -23,3 +23,26 @@ class Event(_EventBase):
     class Config:
         orm_mode = True
 
+#edir defnition
+
+class _EdirBase(BaseModel):
+    name:str
+    payment_frequency: str
+    initial_deposit:int
+    username:   str
+
+class EdirCreate(_EdirBase):
+    pass
+
+class Edir(_EdirBase):
+    id: int
+    owner_id: int
+    events: List[Event] = []
+    
+    class Config:
+        orm_mode = True
+        
+class EdirUpdate(BaseModel):
+    name: str
+    payment_frequency: str
+    initial_deposite: int
