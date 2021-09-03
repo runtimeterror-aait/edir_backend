5a2793c68e3746c2a696a0052c3f2b3db81ab2d5
BgitBB <61750923+BgitBB@users.noreply.github.com>
2021-09-03 23:17:34 +0300

    get and update user

--- app/routers/v1/user.py
+++ app/routers/v1/user.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.repository.auth import AuthHandler
+from app.schemas import UserUpdate
+from app.db.database import get_db
+from app.db.repository.user import get_user_by_email, update_user
 auth_handler = AuthHandler()
 
 
@@ -9,3 +12,13 @@ router = APIRouter(
     tags=["User"],
     responses={404: {"description": "Not found"}},
 )
+
+#get user
+@router.get("/")
+def auth_user(email=Depends(auth_handler.auth_wrapper), db: Session = Depends(get_db)):
+    return get_user_by_email(db=db, email=email)
+
+#update user
+@router.put("/")
+def auth_user_update(user: UserUpdate, email: Depends(auth_handler.auth_wrapper), db: Session = Depends(get_db)):
+    return update_user(db=db, email=email, user=user)
