696f08ffc43f1e713d1e88bbcf31298a16e440ce
davenbezz <50683672+davenbezz@users.noreply.github.com>
2021-09-03 23:38:07 +0300

    created a method for getting all and specific edir

--- app/routers/v1/edir.py
+++ app/routers/v1/edir.py
@@ -12,3 +12,13 @@ router = APIRouter(
     tags=["Manage Edir"],
     responses={404: {"description": "Not found"}},
 )
+
+@router.get("/")
+def get_all_edirs(skip: int = 0, limit: int = 10, email=Depends(auth_handler.auth_wrapper), db: Session = Depends(get_db)):
+    posts = get_edirs(db=db, email=email, skip=skip, limit=limit)
+    return posts
+
+@router.get("/{edir_id}")
+def get_one_edir(edir_id: int, db: Session = Depends(get_db), email=Depends(auth_handler.auth_wrapper)):
+    edir = get_edir_by_id(db=db, id=edir_id)
+    return edir
