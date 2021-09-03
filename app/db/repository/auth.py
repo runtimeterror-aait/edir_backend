3cd62bab0086fa52c86d4de15913005b775f92f1
MIchael Belete <it.michael.belete@gmail.com>
2021-09-03 15:18:15 -0400

    created auth repo and declared auth handler class

---
+++ app/db/repository/auth.py
@@ -0,0 +1,8 @@
+import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+class AuthHandler():
+    pass
