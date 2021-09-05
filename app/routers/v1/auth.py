3a2b9dede98c754903a01850d3a8010d3e86003f
MIchael Belete <it.michael.belete@gmail.com>
2021-09-05 10:29:59 -0400

    add role to login

--- app/routers/v1/auth.py
+++ app/routers/v1/auth.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.db.repository.auth import AuthHandler
 from app.schemas import UserCreate, AuthDetails, User as UserSchema
 from app.db.database import get_db
-from app.db.repository.user import create_user, check_user_exist , check_encrypted_password, get_user_by_email
+from app.db.repository.user import create_user, check_user_exist ,check_encrypted_password, get_user_by_email
 
 auth_handler = AuthHandler()
 
@@ -17,7 +17,8 @@ router = APIRouter(
 def login(auth_details: AuthDetails, db: Session = Depends(get_db)):
     if(check_user_exist(db, auth_details.email, auth_details.password)):
         token = auth_handler.encode_token(auth_details.email)
-        return { 'token': token, 'role': "a" }
+        user = get_user_by_email(db, auth_details.email)
+        return { 'token': token, 'role': user.role }
     else:
         raise HTTPException(status_code=401, detail='Invalid email and/or password')
 
